774a657951519f822c6c571f18373f20774d95c2
Howie Duhzit <Contact@HowieDuhzit.Best>
2025-10-27 00:30:14 -0400

    Remove Redis password environment variable from docker-compose and add DATABASE_URL to production database configuration. Update image source in production environment's content security policy.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https
     policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data, "https://omarchy.org"
+    policy.img_src     :self, :https, :data, "https://omarchy.app"
     policy.object_src  :none
     policy.script_src  :self, :https, "https://cdn.tailwindcss.com"
     policy.style_src   :self, :https, :unsafe_inline
